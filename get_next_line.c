76655250604c3317b2b6a9c782c589c295a3c280
Youri BOUGRE <yobougre@e1r11p6.42.fr>
2021-11-29 17:16:21 +0100

    shit

--- get_next_line.c
+++ get_next_line.c
@@ -33,18 +33,27 @@ char	*ft_super_join(char const *s1, char const *s2)
 	return (output);
 }
 
-int	ft_where_end(char *s)
+char	*ft_where_start(char *s, int limite)
 {
-	int	i;
+	char	*output;
+	int		i;
+	int		j;
 	
 	i = 0;
-	while (s[i])
-	{
-		if (s[i] == '\n')
-			return (i);
+	j = 0;
+	output = malloc(BUFFER_SIZE + 1);
+	if (!output)
+		return (NULL);
+	while (s[i] != '\n')
 		i++;
+	while (i < limite)
+	{
+		output[j] = s[i];
+		++j;
+		++i;
 	}
-	return (0);
+	output[i] = 0;
+	return (output);
 }
 
 char	*get_next_line(int fd)
@@ -56,10 +65,15 @@ char	*get_next_line(int fd)
 
 	while (read(fd, buf, BUFFER_SIZE) )//&& !ft_where_end(output))
 	{
+		buf[BUFFER_SIZE] = 0;
 		save = line;
 		line = ft_strjoin(save, buf);
 		output = ft_super_join(output, line);
 	}
+	line = ft_strchr(line, '\n');
+	printf("line : {%s}\n", line);
+	if (!line)
+		return (NULL);
 	if (!output)
 		return (NULL);
 	return (output);
